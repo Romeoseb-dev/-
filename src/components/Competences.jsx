f924446c3e604faee1f6f2fc6d7ef3cc6d0aa6a8
sebastien <romeo.sebastien.dev@gmail.com>
2023-11-29 15:59:34 +0100

    ajout icons

--- src/components/Competences.jsx
+++ src/components/Competences.jsx
@@ -1,3 +1,6 @@
+import { HiOutlineComputerDesktop } from "react-icons/hi2";
+import { FaReact } from "react-icons/fa";
+import { DiDatabase } from "react-icons/di";
 import "./competences.css";
 
 function Competences() {
@@ -6,13 +9,22 @@ function Competences() {
       <h2 className="mes-competences">Mes Compétences</h2>
       <div className="block-competence">
         <div className="cont-font">
-          <h3 className="font">front-end</h3>
+          <h3 className="font">
+            <FaReact />
+            front-end
+          </h3>
         </div>
         <div className="cont-back">
-          <h3 className="back">back-end</h3>
+          <h3 className="back">
+            <DiDatabase />
+            back-end
+          </h3>
         </div>
         <div className="cont-gest">
-          <h3 className="gestion">collaboration et gestion de projets</h3>
+          <h3 className="gestion">
+            <HiOutlineComputerDesktop />
+            collaboration et gestion de projets
+          </h3>
         </div>
       </div>
     </div>
